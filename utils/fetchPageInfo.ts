eea3b263db013ff6e37650e9e99ce7cbd730105f
Oğulcan Munoğulları <89843337+ogulcanmunogullari@users.noreply.github.com>
2022-10-03 23:57:17 +0400

    env

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,9 +1,7 @@
 import { PageInfo } from "../typings"
 
 export const fetchPageInfo = async () => {
-  const res = await fetch(
-    `https://ogulcanmunogullari.netlify.app/api/getPageInfo`,
-  )
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`)
   const data = await res.json()
   const pageInfo: PageInfo = data
   return pageInfo
--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -1,9 +1,7 @@
 import { Social } from "../typings"
 
 export const fetchSocials = async () => {
-  const res = await fetch(
-    `https://ogulcanmunogullari.netlify.app/api/getSocials`,
-  )
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`)
   const data = await res.json()
   const socials: Social[] = data
   return socials
