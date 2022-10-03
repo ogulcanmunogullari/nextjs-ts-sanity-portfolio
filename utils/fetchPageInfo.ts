87753bdcc34538ee42e9a68b542035c25f8fd036
Oğulcan Munoğulları <89843337+ogulcanmunogullari@users.noreply.github.com>
2022-10-03 23:55:12 +0400

    hardcoding url

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,7 +1,9 @@
 import { PageInfo } from "../typings"
 
 export const fetchPageInfo = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`)
+  const res = await fetch(
+    `https://ogulcanmunogullari.netlify.app/api/getPageInfo`,
+  )
   const data = await res.json()
   const pageInfo: PageInfo = data
   return pageInfo
--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,7 +1,9 @@
 import { Project } from "../typings"
 
 export const fetchProjects = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`)
+  const res = await fetch(
+    `https://ogulcanmunogullari.netlify.app/api/getProjects`,
+  )
   const data = await res.json()
   const projects: Project[] = data
   return projects
